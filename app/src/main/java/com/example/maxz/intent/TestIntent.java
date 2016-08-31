ae3b956c939124413eb4b6285dfec399fa38572f
Teerasak <maxz_drum@icloud.com>
2016-08-31 11:10:45 +0700

    start intent

---
+++ app/src/main/java/com/example/maxz/intent/TestIntent.java
@@ -0,0 +1,15 @@
+package com.example.maxz.intent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestIntent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_intent);
+
+
+    }//main method
+}//main class
